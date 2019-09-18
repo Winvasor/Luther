0e22e4a73e049ea0a3e26d8c92f246a630975c49
Winvasor <55360916+Winvasor@users.noreply.github.com>
2019-09-18 09:08:17 -0300

    Update lut.py

--- lut.py
+++ lut.py
@@ -24,17 +24,23 @@ def main():
    print '  |    |   |  |  \   __\  |  \_/ __ \_  __ \ '
    print '  |    |___|  |  /|  | |   Y  \  ___/|  | \/ '
    print '  |_______ \____/ |__| |___|  /\___ \|__|    '
-   print '          \/                \/     \/     1.1'
+   print '          \/                \/     \/     1.2'
 
 main()
-
+	
 print '[1] LUTHER-GMAIL'
+print '[2] LUTHER-FTP'
 print '[0] SAIR\n'
 OPCAO = input('==>')
 
 if OPCAO == 1:
    system('python2 libs/brute.py')
 
+elif OPCAO == 2:
+	ip = raw_input('INSIRA O HOSTNAME/IP: ')
+	user = raw_input('INSIRA O NOME DE USUARIO: ')
+	PASS = raw_input('INSIRA A WORDLIST: ')
+	system('python2 libs/ftp.py -t '+ip+' -u '+user+' -w '+PASS)
 elif OPCAO == 0:
    print AZ+'\nADEUS!!!'+NOR
    exit()
