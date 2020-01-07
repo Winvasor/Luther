d502e9ca91ef72f4623bdfc952edf9b8b7232520
Winvasor <55360916+Winvasor@users.noreply.github.com>
2020-01-07 12:51:13 -0300

    Update lut.py

--- lut.py
+++ lut.py
@@ -12,22 +12,22 @@ A = '\033[33m'
 AZ = '\033[31m'
 NEGRI = '\033[1m'
 NOR = '\033[0;0m'
-system('clear')   
+system('clear')
 def main():
    print NEGRI+A+'================================================'
    print '               CRIADO POR'+AZ+' WINVASOR'+A
-   print '===================  2019  ====================='
+   print '===================  2020  ====================='
    print '   ____            __  ___                   '
    print '  |    |    __ ___/  |_|  |__   ___________  ' 
    print '  |    |   |  |  \   __\  |  \_/ __ \_  __ \ '
    print '  |    |___|  |  /|  | |   Y  \  ___/|  | \/ '
    print '  |_______ \____/ |__| |___|  /\___ \|__|    '
-   print '          \/                \/     \/     1.5'
+   print '          \/                \/     \/     1.8'
    print AZ+'\a\tRun in Root User'+A
 def indie():	
 	print '[1] LUTHER-GMAIL'
-	print '[2] LUTHER-FTP'
-	print '[3] LUTHER-PORTSCAN'
+	print '[2] LUTHER-PORTSCAN'
+	print '[3] LUTTHER-FACE-BRUTE'
 	print '[4] UPGRADE-LUTHER'
 	print '[0] SAIR\n'
 	
@@ -37,17 +37,17 @@ def indie():
 	   system('python2 libs/brute.py')
 
 	elif OPCAO == "2":
-		ip = raw_input('INSIRA O HOSTNAME/IP: ')
-		user = raw_input('INSIRA O NOME DE USUARIO: ')
-		PASS = raw_input('INSIRA A WORDLIST: ')
-		system('python2 libs/ftp.py -t '+ip+' -u '+user+' -w '+PASS)
-	elif OPCAO == "3":
 		system('python libs/ps.py')
-	elif OPCAO == "0":
-	   print AZ+'\nADEUS!!!\n\n'+NOR
-	   exit()
+	elif OPCAO == "3":
+		system('python libs/fb.py')
+
+
 	elif OPCAO =="4":
 		system('cd .. && rm -r Luther && git clone https://github.com/Winvasor/Luther.git && cd Luther && python2 lut.py')
+	
+	elif OPCAO == "0":
+	   print AZ+'\nADEUS!!!\n\n'+NOR
+	   sys.exit(1)
 	else:
 	   system('clear')
 	   print AZ+'\n#404 NOT FOUND\n'+NOR
