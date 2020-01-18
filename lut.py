df90d4fb5a56a8abfcec039669de433495982b6e
Winvasor <55360916+Winvasor@users.noreply.github.com>
2020-01-18 18:36:28 -0300

    Update lut.py

--- lut.py
+++ lut.py
@@ -5,8 +5,9 @@
 import re
 import sys,os
 import random
+import requests
 import smtplib
-from os import system
+from os import system,getuid
 
 A = '\033[33m'
 AZ = '\033[31m'
@@ -23,7 +24,33 @@ def main():
    print '  |    |___|  |  /|  | |   Y  \  ___/|  | \/ '
    print '  |_______ \____/ |__| |___|  /\___ \|__|    '
    print '          \/                \/     \/     1.8'
-   print AZ+'\a\tRun in Root User'+A
+def checkConnection(host='https://google.com'):  # Connection check
+    system('clear')
+    try:
+        req = requests.get(host, timeout=10)
+        if req.status_code == 200:
+            return True
+    except:
+        return False
+
+
+if checkConnection() == False:
+    print(NEGRI+AZ+'\n\n\a Nenhuma Rede Detectada, conecte-se a internet!!!\n\n')
+    exit()
+
+if getuid() == 0:
+	print('')
+else:
+    print (NEGRI)
+    print(
+    "{0}\aExecute o Luther-Framework no '{1}sudo{0}'".format(AZ, A))
+    print ('\n\n')
+    exit()
+
+
+
+
+
 def indie():	
 	print '[1] LUTHER-GMAIL'
 	print '[2] LUTHER-PORTSCAN'
@@ -40,14 +67,17 @@ def indie():
 		system('python libs/ps.py')
 	elif OPCAO == "3":
 		system('python libs/fb.py')
-
-
 	elif OPCAO =="4":
-		system('cd .. && rm -r Luther && git clone https://github.com/Winvasor/Luther.git && cd Luther && python2 lut.py')
-	
+		system('cd .. && rm -r Luther && git clone https://github.com/Winvasor/Luther.git && cd Luther && python lut.py')
 	elif OPCAO == "0":
 	   print AZ+'\nADEUS!!!\n\n'+NOR
 	   sys.exit(1)
+	elif OPCAO == "":
+		system('clear')
+		main()
+		print(AZ+'\n\nNenhuma alternativa selecionada!!!\n\n'+A)
+		indie()
+		
 	else:
 	   system('clear')
 	   print AZ+'\n#404 NOT FOUND\n'+NOR
